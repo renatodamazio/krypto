edb7a6199475949254510efcac6191bca60e4070
Renato <renatopdamazio@gmail.com>
2022-02-22 11:07:51 -0300

    fix gramatical issues

--- client/src/components/Welcome.tsx
+++ client/src/components/Welcome.tsx
@@ -41,10 +41,10 @@ export default function Welcome() {
       <div className="flex mf:flex-row flex-col items-start justify-between md:p-30 py-20 px-4">
         <div className="flex flex-1 justify-start flex-col mf:mr-10">
           <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
-            Send Cripto <br /> across the world
+            Send Crypto <br /> across the world
           </h1>
           <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
-            Explore the crypto world. Buy and sell criptocurrencies easily on
+            Explore the crypto world. Buy and sell cryptocurrencies easily on
             Krpit
           </p>
           {!currentAccount && (
